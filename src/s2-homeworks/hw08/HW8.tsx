240400aaf8d57939ecf092fdd12a050058511c63
yana-kushmar <yana.puzevich@gmail.com>
2023-04-18 21:47:52 -0400

    hw8

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -19,12 +19,12 @@ export type UserType = {
 
 const initialPeople: UserType[] = [
     // студенты могут поменять имя/возраст/количество объектов, _id должны быть целочисленные
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
+    {_id: 1, name: 'Кот', age: 3},
+    {_id: 2, name: 'Александр', age: 66},
+    {_id: 3, name: 'Коля', age: 16},
+    {_id: 4, name: 'Виктор', age: 44},
+    {_id: 5, name: 'Дмитрий', age: 40},
+    {_id: 6, name: 'Ирина', age: 55},
 ]
 
 const HW8 = () => {
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,27 @@
-import {UserType} from '../HW8'
+import type {UserType} from '../HW8'
+import user from "../User";
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            return state.sort((a: UserType, b: UserType): any => {
+                if(action.payload === 'up') {
 
-            return state // need to fix
+                    return a.name > b.name
+
+                }
+                return b.name > a.name
+            })
+
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(a => a.age >= 18) // need to fix
         }
         default:
             return state
