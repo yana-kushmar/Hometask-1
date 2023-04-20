1079cf1f9b38af973ed5800ed2489eb83997394e
yana-kushmar <yana.puzevich@gmail.com>
2023-04-19 20:52:13 -0400

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,5 +1,5 @@
 import type {UserType} from '../HW8'
-import user from "../User";
+
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
@@ -10,15 +10,13 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            return state.sort((a: UserType, b: UserType): any => {
+            return state.sort((a, b) => {
                 if(action.payload === 'up') {
-
-                    return a.name > b.name
-
+                    return a.name > b.name ? 1 : -1
                 }
-                return b.name > a.name
-            })
 
+                    return b.name > a.name ? 1 : -1
+            })
         }
         case 'check': {
             return state.filter(a => a.age >= 18) // need to fix
